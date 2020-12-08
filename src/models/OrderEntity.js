832022b68e491afcf9a3ed12391c04fbb626b348
Rodrigo Russo <rodrigo.esteves@webjump.com.br>
2020-12-08 16:59:46 -0300

    Handle date and value

--- src/models/OrderEntity.js
+++ src/models/OrderEntity.js
@@ -8,4 +8,11 @@ export class OrderEntity {
         this.date = new Date(`${date}T00:00:00`);
         this.value = value;
     }
+    get dateString() {
+        return this.date.toLocaleDateString('pt-br');
+    }
+
+    get valueString() {
+        return this.value.toLocaleString('pt-br');
+    }
 }
