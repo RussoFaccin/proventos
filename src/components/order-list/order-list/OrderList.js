093280447db999637e6188478e825ac5cd0d7cfb
Rodrigo Russo <rodrigo.esteves@webjump.com.br>
2020-12-09 10:18:04 -0300

    First implementation

--- src/components/order-list/order-list/OrderList.js
+++ src/components/order-list/order-list/OrderList.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+// Models
+import { OrderEntity } from '../../../models/OrderEntity';
 
-const OrderList = () => {
+const OrderList = ({ listEntry = [] }) => {
+    let renderList = listEntry.map((entry, index) => {
+        return (
+            <li className="orderList__row" key={`listKey_${index}`}>
+                <div className="orderList__rowItem">{entry.dateString}</div>
+                <div className="orderList__rowItem">{entry.valueString}</div>
+            </li>
+        );
+    });
+
     return (
         <ul className="orderList">
-            <li className="orderList__row">20/01/2020</li>
+            {renderList}
         </ul>
     );
+}
+
+OrderList.propTypes = {
+    listEntry: PropTypes.arrayOf(OrderEntity)
 }
 
 export default OrderList;
--- src/components/order-list/order-list/OrderList.test.js
+++ src/components/order-list/order-list/OrderList.test.js
@@ -4,17 +4,43 @@ import { render, screen } from '@testing-library/react';
 import OrderList from './OrderList.js';
 // Models
 import { OrderEntity } from '../../../models/OrderEntity';
-import { element } from 'prop-types';
 
 // Mock data
 const mockList = [
-    new OrderEntity('2020-01-20'),
     new OrderEntity('2020-01-20', 10),
     new OrderEntity('2020-02-01', 100.45),
-    new OrderEntity('2020-02-14', 74.45),
     new OrderEntity('2020-02-20', 66.5),
     new OrderEntity('2020-03-15', 20),
+    new OrderEntity('2020-01-20'),
+    new OrderEntity('2020-02-14', 74.45),
 ];
+
+const expectList = [
+    {
+        date: '20/01/2020',
+        value: '10'
+    },
+    {
+        date: '20/01/2020',
+        value: '0'
+    },
+    {
+        date: '01/02/2020',
+        value: '100,45'
+    },
+    {
+        date: '14/02/2020',
+        value: '74,45'
+    },
+    {
+        date: '20/02/2020',
+        value: '66,5'
+    },
+    {
+        date: '15/03/2020',
+        value: '20'
+    },
+]
 
 describe('OrderList component', () => {
     it('Should exist', () => {
@@ -25,19 +51,16 @@ describe('OrderList component', () => {
         render(<OrderList />);
     });
 
-    it('Should render the list', () => {
+    it('Should render the list sorted by date', () => {
         const {container} = render(<OrderList listEntry={mockList} />);
 
         const listItemList = container.querySelectorAll('.orderList__row');
 
-        mockList.forEach((mockData, index) => {
-            console.log(JSON.stringify(mockData));
-        });
-
         expect(listItemList).toBeTruthy();
 
-        listItemList.forEach((itemList, index) => {
-            expect(itemList.innerHTML).toMatch(new RegExp(``, 'i'));
+        expectList.forEach((item, index) => {
+            expect(listItemList[index].innerHTML).toMatch(new RegExp(`${item.date}`, 'i'));
+            expect(listItemList[index].innerHTML).toMatch(new RegExp(`${item.value}`, 'i'));
         });
     });
 });
