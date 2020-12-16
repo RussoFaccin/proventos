23002b00c092f47ca343ecb663d3d6e3346f84c5
Rodrigo Russo <rodrigo.esteves@webjump.com.br>
2020-12-16 12:12:08 -0300

    Add type property

--- src/models/OrderEntity.js
+++ src/models/OrderEntity.js
@@ -3,10 +3,12 @@ import { DateUtils } from '../utils/DateUtils';
 export class OrderEntity {
     constructor(
         date = `${DateUtils.getTodayFormated()}`,
-        value = 0
+        value = 0,
+        type = 'provento'
     ) {
         this.date = new Date(`${date}T00:00:00`);
         this.value = value;
+        this.type = type;
     }
     get dateString() {
         return this.date.toLocaleDateString('pt-br');
