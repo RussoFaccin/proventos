7c8e3f4ebcd1884c1ff1f360c2cee2479335afdf
Rodrigo Russo <rodrigo.esteves@webjump.com.br>
2020-12-21 16:54:42 -0300

    Add month labels to return

--- src/utils/ChartData.js
+++ src/utils/ChartData.js
@@ -1,7 +1,10 @@
+
+import { DateUtils } from '../utils/DateUtils';
+
 export class ChartData {
     static createData(listEntry, yearEntry) {
         const tmpList = {
-            labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+            labels: DateUtils.getMonthNames(),
             data: [250.97, 110.45, 0, 0, 0, 0, 0, 200, 0, 52, 0, 10]
         }
         
