ac2c3099d2a4824c879b1f5b408ace677d0baf4e
Rodrigo Russo <rodrigo.esteves@webjump.com.br>
2020-12-21 18:11:26 -0300

    Add method to filter data by year

--- src/utils/ChartData.js
+++ src/utils/ChartData.js
@@ -1,13 +1,27 @@
-
+import { OrderEntity } from '../models/OrderEntity';
 import { DateUtils } from '../utils/DateUtils';
 
 export class ChartData {
     static createData(listEntry, yearEntry) {
-        const tmpList = {
-            labels: DateUtils.getMonthNames(),
-            data: [250.97, 110.45, 0, 0, 0, 0, 0, 200, 0, 52, 0, 10]
-        }
-        
-        return tmpList;
+        const dataInYear = ChartData.filterByYear(listEntry, yearEntry)
+
+        DateUtils.getMonthNames().forEach()
+        // const tmpList = {
+        //     labels: DateUtils.getMonthNames(),
+        //     data: [250.97, 110.45, 0, 0, 0, 0, 0, 200, 0, 52, 0, 10]
+        // }
     }
+
+    /**
+     * Filter entry by year
+     * @param {OrderEntity[]} listEntry
+     * @param {string} yearEntry year to filter
+     * @return {OrderEntity[]}
+     */
+    static filterByYear(listEntry, yearEntry) {
+        return listEntry.filter((entry) => {
+            return entry.date.getFullYear() === Number(yearEntry);
+        });
+    }
+    
 }
--- src/utils/ChartData.test.js
+++ src/utils/ChartData.test.js
@@ -2,29 +2,43 @@ import { ChartData } from './ChartData';
 import { OrderEntity } from '../models/OrderEntity';
 
 describe('ChartData', () => {
+    const entryList = [
+        new OrderEntity(null, '2019-08-05', 200),
+        new OrderEntity(null, '2020-08-05', 200),
+        new OrderEntity(null, '2020-01-02', 100.5),
+        new OrderEntity(null, '2020-01-01', 100),
+        new OrderEntity(null, '2020-01-14', 50.47),
+        new OrderEntity(null, '2020-02-10', 60.45),
+        new OrderEntity(null, '2020-12-31', 10),
+        new OrderEntity(null, '2020-02-10', 50),
+        new OrderEntity(null, '2020-10-10', 52),
+    ];
+
     it('Should exists', () => {
         expect(ChartData).toBeDefined();
     });
 
-    it('Should create data structure given OrderEntity[]', () => {
-        const entryList = [
-            new OrderEntity(null, '2020-08-05', 200),
-            new OrderEntity(null, '2020-01-02', 100.5),
-            new OrderEntity(null, '2020-01-01', 100),
-            new OrderEntity(null, '2020-01-14', 50.47),
-            new OrderEntity(null, '2020-02-10', 60.45),
-            new OrderEntity(null, '2020-12-31', 10),
-            new OrderEntity(null, '2020-02-10', 50),
-            new OrderEntity(null, '2020-10-10', 52),
-        ]
+    it('Should filter entry by year', () => {
+        const dataInYear2019 = ChartData.filterByYear(entryList, '2019');
+        expect(dataInYear2019.length).toBe(1);
 
-        const formatedList = ChartData.createData(entryList, '2020');
+        const dataInYear2020 = ChartData.filterByYear(entryList, '2020');
+        expect(dataInYear2020.length).toBe(8);
 
-        const expectedList = {
-            labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-            data: [250.97, 110.45, 0, 0, 0, 0, 0, 200, 0, 52, 0, 10]
-        };
+        const dataInYear2010 = ChartData.filterByYear(entryList, '2010');
+        expect(dataInYear2010.length).toBe(0);
+    })
 
-        expect(formatedList).toEqual(expectedList);
-    });
+    // it('Should create data structure given OrderEntity[]', () => {
+    
+
+    //     const formatedList = ChartData.createData(entryList, '2020');
+
+    //     const expectedList = {
+    //         labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+    //         data: [250.97, 110.45, 0, 0, 0, 0, 0, 200, 0, 52, 0, 10]
+    //     };
+
+    //     expect(formatedList).toEqual(expectedList);
+    // });
 });
