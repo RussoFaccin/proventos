0cdbc1494583aace3186638907eb520d84b1d319
Rodrigo Russo <rodrigo.esteves@webjump.com.br>
2020-12-23 17:59:11 -0300

    Add fields and buttons

--- src/components/app-form/AppForm.js
+++ src/components/app-form/AppForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 // Models
 import { FormEntity } from '../../models/FormEntity';
@@ -8,22 +8,67 @@ import { FormEntity } from '../../models/FormEntity';
  * @param {Object} props
  * @param {FormEntity[]} props.fieldList
  */
-export default function AppForm({...props}) {
+export default function AppForm({
+    fieldList = new FormEntity({}),
+    buttonList = [{
+        value: 'Salvar',
+        action: () => false
+    }]
+}) {
     // Methods
-    /**
-     * Create form state object
-     * @param {FormEntity[]} fieldListEntry 
-     */
     function mountFormState(fieldListEntry) {
         const formState = {
             formGroup: {}
         };
+
+        fieldListEntry.forEach((field) => {
+            if (!(field.name in formState.formGroup)) {
+                formState.formGroup[field.name] = field
+            }
+        });
+
+        return formState;
     }
+
+    function handleChange(evt) {
+        const tmpState = state;
+        tmpState.formGroup[evt.target.name].value = evt.target.value;
+
+        setState({
+            ...state,
+            tmpState
+        });
+    };
+
+    // State
+    const [state, setState] = useState(mountFormState(fieldList));
+
+    const formFields = Object.entries(state.formGroup).map(([key, value], index) => {
+        return (
+            <section className="formRow" key={`row-${index}`}>
+                {value.label ? <label htmlFor={key}>{value.label}</label> : null}
+                <input
+                    type={value.type}
+                    id={key}
+                    name={key}
+                    pattern={value.pattern}
+                    value={value.value}
+                    onChange={handleChange}
+                    required={value.required}
+                />
+            </section>
+        );
+    });
+    
+    const buttonBox = buttonList.map((button, index) => {
+        return <button className="prvButton" key={`button_${index}`} onClick={button.action}>{button.value}</button>
+    })
 
     // Render
     return (
         <form className="appForm">
-            
+            {formFields}
+            {buttonBox}
         </form>
     );
 }
