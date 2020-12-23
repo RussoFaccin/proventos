07e00a2bdd729c1ebb1ebd2380505a28230f9339
Rodrigo Russo <rodrigo.esteves@webjump.com.br>
2020-12-23 14:46:28 -0300

    First implementation

--- src/components/app-form/AppForm.js
+++ src/components/app-form/AppForm.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export default function AppForm() {
+// Models
+import { FormEntity } from '../../models/FormEntity';
+
+/**
+ * AppForm Component
+ * @param {Object} props
+ * @param {FormEntity[]} props.fieldList
+ */
+export default function AppForm({...props}) {
+    // Methods
+    /**
+     * Create form state object
+     * @param {FormEntity[]} fieldListEntry 
+     */
+    function mountFormState(fieldListEntry) {
+        const formState = {
+            formGroup: {}
+        };
+    }
+
+    // Render
     return (
         <form className="appForm">
             
--- src/components/app-form/AppForm.test.js
+++ src/components/app-form/AppForm.test.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {render, screen} from '@testing-library/react';
 import AppForm from './AppForm';
 
+//Models
+import { FormEntity } from '../../models/FormEntity';
+
 describe('AppForm component', () => {
     it('Should exists', () => {
         expect(AppForm).toBeDefined();
@@ -9,5 +12,27 @@ describe('AppForm component', () => {
 
     it('Should render the component', () => {
         render(<AppForm/>);
-    })
+    });
+
+    it('Should render fields', () => {
+        const fieldList = [
+            new FormEntity({
+                label: 'Date',
+                type: 'date',
+                required: true,
+            }),
+            new FormEntity({
+                label: 'Value',
+                type: 'text',
+                required: true,
+                pattern: '[0-9]+([,.]+[0-9]+)?'
+            }),
+        ];
+
+        render(<AppForm fieldList={fieldList} />);
+
+        fieldList.forEach((field) => {
+            screen.getByLabelText(new RegExp(field.label, 'i'));
+        })
+    });
 });
